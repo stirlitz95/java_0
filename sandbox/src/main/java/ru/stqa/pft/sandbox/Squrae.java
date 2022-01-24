d350115d44d42b27b297615815a6dab8f00e640f
Юрий Антропов <stirlitz95@mail.ru>
2022-01-24 22:47:57 +0300

    Update Squrae.java

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Squrae.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Squrae.java
@@ -4,11 +4,11 @@ public class Squrae {
 
   public double l;
 
-  public Squrae(double len){
+  public Squrae(double len) {
     this.l = len;
   }
 
-  public  double area () {
+  public double area() {
     return this.l * this.l;
   }
 
