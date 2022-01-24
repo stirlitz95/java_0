3999f7d1278ebff53925b768d401428926543cc1
Юрий Антропов <stirlitz95@mail.ru>
2022-01-24 12:59:22 +0300

    Изменены настройки форматирования

--- sandbox/src/main/java/ru/stqa/pft/sandbox/TEST.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/TEST.java
@@ -2,7 +2,7 @@ package ru.stqa.pft.sandbox;
 
 public class TEST {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Проверка настройки окружения");
     }
 
