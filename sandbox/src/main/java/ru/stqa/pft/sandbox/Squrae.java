88119c95d70340f0dba02ced9f438e6c339247f1
Юрий Антропов <stirlitz95@mail.ru>
2022-01-24 20:28:39 +0300

    Добавлены функции ассоциированные с объектами

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Squrae.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+public class Squrae {
+
+  public double l;
+
+  public Squrae(double len){
+    this.l = len;
+  }
+
+  public  double area () {
+    return this.l * this.l;
+  }
+
+}
