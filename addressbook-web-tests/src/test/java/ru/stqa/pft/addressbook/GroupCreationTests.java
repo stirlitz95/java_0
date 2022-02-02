8c7e3fcf344600f1b40fcf89dc84477b2ac681ab
Юрий Антропов <stirlitz95@mail.ru>
2022-02-02 20:20:45 +0300

    отрефакторил тест на создание группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -39,7 +39,7 @@ public class GroupCreationTests {
   public void testGroupCreation() throws Exception {
     gotoGroupPage();
     initGroupCreation();
-    fillGroupFrom("555", "555", "555");
+    fillGroupFrom(new GroupData("555", "555", "555"));
     submitGroupCreation();
     returnToGroupPage();
   }
@@ -52,16 +52,16 @@ public class GroupCreationTests {
     wd.findElement(By.linkText("group page")).click();
   }
 
-  private void fillGroupFrom(String name, String header, String footer) {
+  private void fillGroupFrom(GroupData groupData) {
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys(name);
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
     wd.findElement(By.name("group_header")).click();
     wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys(header);
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
     wd.findElement(By.name("group_footer")).click();
     wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys(footer);
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
     wd.findElement(By.name("submit")).click();
   }
 
