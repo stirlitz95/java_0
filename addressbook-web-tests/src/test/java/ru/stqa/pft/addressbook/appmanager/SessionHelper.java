114f39139ca1d6d97193ab61d97fc341615fdf8f
Юрий Антропов <stirlitz95@mail.ru>
2022-02-06 22:04:03 +0300

    все тесты по лекцию 2.8 преобразованы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -6,18 +6,14 @@ import org.openqa.selenium.WebDriver;
 public class SessionHelper extends HelperBase {
 
   public SessionHelper(WebDriver wd) {
-
     super(wd);
   }
 
   public void login(String username, String password) {
     wd.get("http://localhost/addressbook/group.php");
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
+    type(By.name("user"), username);
+    type(By.name("pass"), password);
+    click(By.xpath("//input[@value='Login']"));
   }
 
 }
