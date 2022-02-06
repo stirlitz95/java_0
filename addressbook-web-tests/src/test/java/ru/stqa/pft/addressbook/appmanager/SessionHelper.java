33f014da8c7d3c7f58cbffc40c97b0e97e3fef92
Юрий Антропов <stirlitz95@mail.ru>
2022-02-06 21:48:56 +0300

    до этого момента GroupCreationTests работает, собираюсь править login в SessionHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -3,12 +3,11 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class SessionHelper {
-  private WebDriver wd;
+public class SessionHelper extends HelperBase {
 
   public SessionHelper(WebDriver wd) {
 
-    this.wd = wd;
+    super(wd);
   }
 
   public void login(String username, String password) {
