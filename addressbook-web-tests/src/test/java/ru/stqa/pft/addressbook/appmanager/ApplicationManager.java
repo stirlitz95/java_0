8028089c8a8f3087995d02a5a6b95f91294d30df
Юрий Антропов <stirlitz95@mail.ru>
2022-02-09 21:27:59 +0300

    создан класс Equality sandbox для сравнение объектов при помощи оператора == и метода equals
    "эта запись только для пробы"

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -22,13 +22,13 @@ public class ApplicationManager {
   }
 
   public void init() {
-    if (browser == BrowserType.CHROME){
+    if (browser.equals(BrowserType.CHROME)){
       wd = new ChromeDriver();
-    } else if (browser == BrowserType.FIREFOX){
+    } else if (browser.equals(BrowserType.FIREFOX)){
       wd = new FirefoxDriver();
-    }else if (browser == BrowserType.IE){
+    }else if (browser.equals(BrowserType.IE)){
       wd = new InternetExplorerDriver();
-    }else if (browser == BrowserType.EDGE){
+    }else if (browser.equals(BrowserType.EDGE)){
       wd = new EdgeDriver();
     }
 
